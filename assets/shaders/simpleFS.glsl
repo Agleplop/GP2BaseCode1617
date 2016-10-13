f98b1e38bf0c7acf5bbd1331fc5dfa9ee236a076
Agleplop <ADUNCA202@caledonian.ac.uk>
2016-10-13 12:46:36 +0100

    Finished

--- assets/shaders/simpleFS.glsl
+++ assets/shaders/simpleFS.glsl
@@ -1,8 +1,9 @@
 #version 410
 
 out vec4 FragColor;
+uniform vec4 Colour = (1.0f,0.0f,0.0f,0.0f);
 
 void main()
 {
-	FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+	FragColor = Colour;
 }
--- src/MyGame.cpp
+++ src/MyGame.cpp
@@ -39,11 +39,31 @@ void MyGame::initScene()
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,sizeof(Vertex),NULL);
+
+	GLuint vertexShaderProgram = 0;
+	std::string vspath = ASSET_PATH+SHADER_PATH+"\simpleVS.glsl";
+	vertexShaderProgram = loadShaderFromFile(vspath, VERTEX_SHADER);
+
+	GLuint fragmentShaderProgram = 0;
+	std::string fsPath = ASSET_PATH + SHADER_PATH + "\simpleFS.glsl";
+	fragmentShaderProgram = loadShaderFromFile(fsPath, FRAGMENT_SHADER);
+
+	m_ShaderProgram = glCreateProgram();
+	glAttachShader(m_ShaderProgram, vertexShaderProgram);
+	glAttachShader(m_ShaderProgram, fragmentShaderProgram);
+	glLinkProgram(m_ShaderProgram);
+	checkForLinkErrors(m_ShaderProgram);
+	logShaderInfo(m_ShaderProgram);
+
+	glDeleteShader(vertexShaderProgram);
+	glDeleteShader(fragmentShaderProgram);
 }
 
 void MyGame::destroyScene()
 {
 	GameApplication::destroyScene();
+
+	glDeleteProgram(m_ShaderProgram);
 	glDeleteVertexArrays(1, &m_VAO);
 	glDeleteBuffers(1, &m_VBO);
 }
@@ -51,9 +71,33 @@ void MyGame::destroyScene()
 void MyGame::render()
 {
 	GameApplication::render();
+
+	glUseProgram(m_ShaderProgram);
+	glBindVertexArray(m_VAO);
+
+	GLint MVPLocation = glGetUniformLocation(m_ShaderProgram, "MVP");
+	if (MVPLocation != -1)
+	{
+		mat4 MVP = m_ProjMatrix*m_ViewMatrix*m_ModelMatrix;
+		glUniformMatrix4fv(MVPLocation, 1, GL_FALSE, glm::value_ptr(MVP));
+	}
+
+	GLint ColourLocation = glGetUniformLocation(m_ShaderProgram, "Colour");
+	if (ColourLocation != -1)
+	{
+		vec4 Colours = vec4 (1.0f, 0.0f, 0.0f, 0.0f);
+		glUniform4fv(ColourLocation, 1, glm::value_ptr(Colours));
+	}
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
 void MyGame::update()
 {
 	GameApplication::update();
+
+	m_ProjMatrix = perspective(radians(45.0f), (float)m_WindowWidth / (float)m_WindowHeight, 0.1f, 100.0f);
+	m_ViewMatrix = lookAt(vec3(0.0f, 0.0f, 10.0f), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f));
+	m_ModelMatrix = translate(mat4(1.0f), vec3(0.0f, 0.0f, -1.0f));
+
+
 }
