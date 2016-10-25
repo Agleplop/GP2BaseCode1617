d899a102423f856fa951637306344085e7b0c042
Agleplop <ADUNCA202@caledonian.ac.uk>
2016-10-25 14:55:48 +0100

    End of part 3 of Ex5

--- src/MyGame.cpp
+++ src/MyGame.cpp
@@ -11,12 +11,11 @@ MyGame::MyGame()
 
 MyGame::~MyGame()
 {
-
+	m_TestObject = nullptr;
 }
 
 void MyGame::initScene()
 {
-	/*
 	Vertex verts[] = {
 		{vec3(-0.5f, -0.5f, 0.0f), vec4(1.0f,1.0f,1.0f,1.0f), vec2(0.0f,3.0f)},
 		{vec3(0.5f, -0.5f, 0.0f), vec4(1.0f,1.0f,1.0f,1.0f), vec2(3.0f,3.0f)},
@@ -25,21 +24,31 @@ void MyGame::initScene()
 		{vec3(0.5f, 0.5f, 0.0f), vec4(1.0f,1.0f,1.0f,1.0f), vec2(3.0f,0.0f)},
 		{vec3(0.5f, -0.5f, 0.0f), vec4(1.0f,1.0f,1.0f,1.0f), vec2(3.0f,3.0f)}
 	};
-	*/
 
-
+	m_TestObject = new GameObject();
 
 	string vsPath = ASSET_PATH + SHADER_PATH + "/simpleVS.glsl";
 	string fsPath = ASSET_PATH + SHADER_PATH + "/textureFS.glsl";
 
+	m_TestObject->LoadShaders(vsPath, fsPath);
+
+
 	//lets load texture
 	string texturePath = ASSET_PATH + TEXTURE_PATH + "/texture.png";
+
+	m_TestObject->LoadTexture(texturePath);
 	
+	m_TestObject->CopyVertexData(verts, 6);
 }
 
 void MyGame::destroyScene()
 {
-
+	if (m_TestObject)
+	{
+		m_TestObject->OnDestroy();
+		delete m_TestObject;
+		m_TestObject = nullptr;
+	}
 }
 
 void MyGame::update()
@@ -49,9 +58,12 @@ void MyGame::update()
 	m_ProjMatrix = perspective(radians(45.0f), (float)m_WindowWidth / (float)m_WindowHeight, 0.1f, 100.0f);
 	m_ViewMatrix = lookAt(vec3(0.0f, 0.0f, 10.0f), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f));
 	m_ModelMatrix = translate(mat4(1.0f), vec3(0.0f, 0.0f, -0.2f));
+
+	m_TestObject->OnUpdate();
 }
 
 void MyGame::render()
 {
 	GameApplication::render();
+	m_TestObject->OnRender(m_ViewMatrix, m_ProjMatrix);
 }
